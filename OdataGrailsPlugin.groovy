8e2795b9030b696151bb5339f34be6be48dd864c
Alex Gitlin <alex.gitlin@gmail.com>
2013-04-05 15:26:55 -0400

    added descriptions and urls

--- OdataGrailsPlugin.groovy
+++ OdataGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class OdataGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
@@ -11,32 +11,43 @@ class OdataGrailsPlugin {
     ]
 
     // TODO Fill in these fields
-    def title = "Odata Plugin" // Headline display name of the plugin
+    def title = "Open Data Viewer" // Headline display name of the plugin
     def author = "Alex Gitlin, Artem Portnoy"
-    def authorEmail = "alex.gitlin@gmail.com"
+    def authorEmail = "speakers.meet@gmail.com"
     def description = '''\
-Add table views of oData - based domain objects. The UI comes from Twitter Bootstrap
+Grails Plugin for building Open Data Protocol - based table views on top of Twitter Bootstrap. 
+
+Installation: 
+1) install kickstart-with-bootstrap plugin
+2) run kickstart script
+3) install odata plugin
+4) (optional) add the following line to the init closure of your BootStrap.groovy: 
+org.grails.plugin.odata.Table.addSamples()
+This should add two sample tables to your application. Use the supplied Table and Column controllers to change the views.
+
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/odata"
+    def documentation = "https://github.com/agitlin/odata/"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Alex Gitlin", email: "speakers.meet@gmail.com" ],
+						[ name: "Artem Portnoy", email: "speakers.meet@gmail.com" ]
+						]
 
     // Location of the plugin's issue tracker.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/agitlin/odata/" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
