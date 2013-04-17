28e320aed4942bf9388f8d3795f0a2ce9cff3c8d
Alex Gitlin <alex.gitlin@gmail.com>
2013-04-17 11:25:18 -0400

    refactoring editor

--- web-app/js/odataViewEditor.js
+++ web-app/js/odataViewEditor.js
@@ -1,3 +1,6 @@
+var sourceId;
+	
+
 function registerDeleter() {
 	$(".deleter").click(function (e) {
 		var rm= $(this).parent().remove();
@@ -28,84 +31,95 @@ function hideAll() {
 	hideColumns()
 }
 
+
 function showAll() {
 	$("#entityTypes").show()
 	$('#columns').show();
 	$('#name').show();
 }
 
+var onSourceChange =function () {
+		var entityURL=root+'source/getEntityTypes/';
+
+		hideAll()
+		sourceId=$("#source").val()
+		if (sourceId =="null") {
+			return
+		} 
+		$.getJSON(entityURL+sourceId, function(data) {
+			var items = [];
+	 
+			items.push('<option value="">Select one...</option>');
+			$.each(data, function( key,val) {
+				items.push('<option value="' + val + '">' + val + '</option>');
+			});
+	 
+			
+			$('#entitySelector').append(items.join(''));
+			$("#entityTypes").show()
+		});
+	
+}
+
+var onTableChange= function () {
+	var propertyURL=root+'source/getProperties/';
+	hideColumns()
+
+	var eType=$("#entitySelector").val();
+	if (eType =="") {
+		return
+	} 
+	$('#name').show();
+	$('#name input').val(eType.replace(/([A-Z])/g, ' $1')+'s');	
+
+	$.getJSON(propertyURL+sourceId+'?entityType='+eType, function(data) {
+		var items = [];
+		var excludedItems=[];
+			 
+		$.each(data, function( key,val) {
+			items.push('<li class="ui-state-default" ><input type="hidden" name="prop" value="'+val+'"/>'  + val + '<div class="icon-remove deleter" id='+val+'></div></li>');
+			
+		});
+		$('#sortable ul').prepend(items.join(''));
+		$('#excluded ul').prepend(excludedItems.join(''));
+		$("#excluded").hide()
+		registerDeleter();
+		$('#columns').show();
+	});
+};
+
+var connectSortableLists = function() {
+	$( "ul.props" ).sortable({
+		connectWith:"#sortable ul",
+	    stop: function(event, ui) {
+	    		 toggleExcluded()
+				 if (ui.item.parent().parent().attr("id")=="sortable"){
+		           ui.item.find("div").addClass("icon-remove");
+		           registerDeleter();
+		           ui.item.find("input").attr("name","prop")
+				 }
+				 
+				 
+		}
+	}).disableSelection();
+	registerDeleter()
+	if (actionName=="edit") {
+		$("#columns").show()
+	}
+	
+
+}
+
 $(document).ready(function() {
 	
-	var sourceId;
-	var entityURL=root+'source/getEntityTypes/';
-	var propertyURL=root+'source/getProperties/';
 	if (actionName=="create") {
 		hideAll()
 	}
 	else {
 		showAll()
 	}
-		$("#source").change( function () {
-			hideAll()
-			sourceId=$("#source").val()
-			if (sourceId =="null") {
-				return
-			} 
-			$.getJSON(entityURL+sourceId, function(data) {
-				var items = [];
-		 
-				items.push('<option value="">Select one...</option>');
-				$.each(data, function( key,val) {
-					items.push('<option value="' + val + '">' + val + '</option>');
-				});
-		 
-				
-				$('#entitySelector').append(items.join(''));
-				$("#entityTypes").show()
-			});
-		});
+	$("#source").change( onSourceChange);
 	
-	$("#entitySelector").change( function () {
-		hideColumns()
-
-		var eType=$("#entitySelector").val();
-		if (eType =="") {
-			return
-		} 
-		$('#name').show();
-		$('#name input').val(eType.replace(/([A-Z])/g, ' $1')+'s');	
-
-		$.getJSON(propertyURL+sourceId+'?entityType='+eType, function(data) {
-			var items = [];
-			var excludedItems=[];
-				 
-			$.each(data, function( key,val) {
-				items.push('<li class="ui-state-default" ><input type="hidden" name="prop" value="'+val+'"/>'  + val + '<div class="icon-remove deleter" id='+val+'></div></li>');
-				
-			});
-			$('#sortable ul').prepend(items.join(''));
-			$('#excluded ul').prepend(excludedItems.join(''));
-			$("#excluded").hide()
-			registerDeleter();
-			
-		});
-	});
-	$(function() {
-		$( "ul.props" ).sortable({
-			connectWith:"#sortable ul",
-		    stop: function(event, ui) {
-		    		 toggleExcluded()
-					 if (ui.item.parent().parent().attr("id")=="sortable"){
-			           ui.item.find("div").addClass("icon-remove");
-			           registerDeleter();
-			           ui.item.find("input").attr("name","prop")
-					 }
-					 
-					 
-			}
-		}).disableSelection();
-		registerDeleter()
-		$("#columns").show()
-
-	});
+	$("#entitySelector").change(onTableChange);
+	$(connectSortableLists);
 })
